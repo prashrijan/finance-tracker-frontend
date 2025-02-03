33c18ca1184a371e41f0cfac3d3f8afde5ec7e50
Prashrijan <shresthaprashrijan@gmail.com>
2025-02-04 09:48:23 +1100

    fix loader height

--- src/Others/SkeletonLoader.jsx
+++ src/Others/SkeletonLoader.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const SkeletonLoader = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-full">
       <main className="flex-grow flex items-center justify-center">
         <motion.div
           className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full"
